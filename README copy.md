11f49792902c71e6e0ea4afeb2977f87ae666bfc
Federico Traiman <federico.traiman@travelgenio.com>
2021-08-05 14:17:31 -0300

    Refactor packages

--- README copy.md
+++
@@ -1,3 +0,0 @@
-# appswing lolo sas
-
-lele
--- src/edu/innova/main/main.java
+++ src/edu/innova/main/main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.innova;
+package edu.innova.main;
 
 /**
  *
@@ -15,7 +15,9 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("edu.innova.main.main()");
+        
+        
     }
     
 }
