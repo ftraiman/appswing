124712cce1095898ed9fbe35b8c701649dd53900
Federico Traiman <federico.traiman@travelgenio.com>
2021-08-11 17:10:28 -0300

    Limpieza main

--- src/edu/innova/main/main.java
+++ src/edu/innova/main/main.java
@@ -1,32 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.innova.main;
 
-import edu.innova.logica.Constantes;
-import edu.innova.logica.controladores.UsuarioControlador;
-import edu.innova.logica.controladores.impl.UsuarioControladorImpl;
-import edu.innova.logica.entidades.Artista;
-import edu.innova.logica.entidades.Espectador;
-import edu.innova.logica.entidades.Usuario;
-import edu.innova.logica.servicios.UsuarioServicio;
-import edu.innova.logica.servicios.impl.UsuarioServicioImpl;
-import java.sql.SQLException;
-import java.util.Date;
-import java.util.List;
 
-/**
- *
- * @author federico
- */
 public class main {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
     }
 
